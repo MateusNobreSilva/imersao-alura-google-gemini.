6fe6083317ad1423d7e5f8ec774c376aafff1c91
Mateus_Nobre <58150931+MateusNobreSilva@users.noreply.github.com>
2024-09-06 12:53:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+
+// console.log(dados);
+
+
+
+function pesquisar() {
+
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um personagem ou característica</p>";
+
+        return;
+    }
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+
+    for (let dado of dados) {
+
+        // console.log(dado.titulo.includes(campoPesquisa));
+
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+                <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">
+                    See more</a>
+            </div>`;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>";
+    }
+
+    section.innerHTML = resultados;
+
+}
---
+++ dados.js
@@ -0,0 +1,101 @@
+// let dados = "D'arce";
+let dados = [
+    {
+        titulo: 'Cahara',
+        descricao: 'He originates from the Eastern Sanctuaries. A bastard child left on his own at a very young age. Somehow this poor soul found his way to these dungeon...',
+        link: 'https://fearandhunger.wiki.gg/wiki/Cahara',
+        tags: 'Mercenary eastern playable character'
+    },
+    {
+        titulo: "D'arce",
+        descricao: 'The trusted knight of the propheticed one. She seems awfully dim standing next to the blinding shimmer of the one... But it might be wrong to misjudge her potential...',
+        link: 'https://fearandhunger.wiki.gg/wiki/D%27arce',
+        tags: 'knight woman playable character'
+    },
+    {
+        titulo: 'Enki',
+        descricao: 'One of the more notorious dark priests of the modern age. We would not be surprised if he was to rise and become the new enlightened one.',
+        link: 'https://fearandhunger.wiki.gg/wiki/Enki',
+        tags: 'priests dark playable character'
+    },
+    {
+        titulo: 'Ragnvaldr',
+        descricao: 'An intimidating figure for sure. Seems like even a northern brute like him can appreciate finesse and knowledge.',
+        link: 'https://fearandhunger.wiki.gg/wiki/Ragnvaldr',
+        tags: 'ultra violence playable character'
+    },
+    {
+        titulo: "Marriage",
+        descricao: "You have become a materialization of the love you two shared. Intervined forever. What will you call yourself from now on?",
+        link: 'https://fearandhunger.wiki.gg/wiki/Marriage',
+        tags: 'marriage playable character'
+    },
+    {
+        titulo: "Marriage (Fusion)",
+        descricao: "Sylvian has blessed you with the marriage of flesh.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Marriage',
+        tags: 'marriage fusion playable character'
+    },
+    {
+        titulo: "Le'garde",
+        descricao: "The prophesied one... Already while still alive, there were many tales written and sung about his tale. We don't know where the prophecy got started but needless to say that it's all fallacy. But the man himself... Is a curious kind. There is definitely something different about him and whatever his part might be in the greater scheme of things, the very least he started something larger...",
+        link: 'https://fearandhunger.wiki.gg/wiki/Le%27garde',
+        tags: 'prophecy playable character'
+    },
+    {
+        titulo: "Nas'hrah",
+        descricao: "The previous new god who held the libraries as his own after the great Betel. How Betel fell for the pathetic tricks Nas'hrah pulled against him is a complete mystery to us. Nas'hrah was not content with what we have here. That miserable worm... The old fool... His mind was weak, his body was weak and most of all, he couldn't hold his own against the pressure this place has on people. Without reason, he used to sodomize everyone who stood in his way... That is how he desired to spend his days instead of the true godhood we have here.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Nas%27hrah',
+        tags: 'head fly playable character'
+    },
+    {
+        titulo: "Girl",
+        descricao: "A pure being, in which both strength of the new gods and a seed of the man from the prophecies come together. Never in the written history has there been similar situation. It is truly exciting, isn't it? We can expect great things from her.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Girl',
+        tags: 'kid god fear & hunger playable character'
+    },
+    {
+        titulo: "Kid Demon",
+        descricao: "I know of the 1st secret of Sylvian: If I sprinkle my seed inside the womb of a freshly deceased, the life that is left lingering in that corpse will sprout a son in my name. A son that will a life of violence.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Kid_Demon',
+        tags: 'kid demon playable character'
+    },
+    {
+        titulo: "Moonless",
+        descricao: "A lone cave wolf who was once driven away from her pack because of her superior strength. The black has its way of changing the nature of wolves.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Moonless',
+        tags: 'dog wolf playable character'
+    },
+    {
+        titulo: "Ghoul",
+        descricao: "Those whose physical and mental fortitude couldn’t hand the ever-pressing darkness. They lost their mind.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Ghoul',
+        tags: 'zombie death playable character'
+    },
+    {
+        titulo: "Skeleton",
+        descricao: "A simple reanimated skeleton. The necromancy needs to be stronger in them to animate mere dust and bones, thus their strength is also greater than ghouls.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Skeleton',
+        tags: 'death bones playable character'
+    },
+    {
+        titulo: "Blood golem",
+        descricao: "Sacrifice blood to summon a golem that fights by your side temporarily. The golem only takes orders from the one that gave birth to it.",
+        link: 'https://fearandhunger.wiki.gg/wiki/Blood_golem',
+        tags: 'golem blood playable character'
+    }
+];
+
+
+
+
+// console.log(dados[1].titulo)
+
+
+// let obbjjaa = {
+//     id: 1,
+//     nome: 'Mateus',
+//     idade: 26
+// }
+
+// console.log(obbjjaa.id)
